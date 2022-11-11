832beae4116f66e862b27957a794014749abf43d
Kirandas96 <parvathikiran10@gmail.com>
2022-11-11 22:49:36 +0530

    done

--- Routes/BookRoute.js
+++ Routes/BookRoute.js
@@ -12,14 +12,22 @@ BookRouter.get("/:userId", async (req, res) => {
     {$group:{_id:null,boat:{$push:"$$ROOT"}}},
     {$unwind:'$boat'},
     {$project:{boat:1,distance:{$abs:{$subtract:[user.location,'$boat.location']}},rating:{$abs:{$subtract:[user.rating,'$boat.rating']}},rides:{$abs:{$subtract:[user.rides,'$boat.rides']}}}}
-    ,{$project:{boat:1,locationPoints: {$switch: {
+    ,{$project:{boat:1,
+        locationPoints: {$switch: {
         branches: [
            { case: { $lte:[ "$distance", 3 ]}, then: 5 },
-        //    { case: { $eq: ["$distance",4] }, then: 3 },
-           { case: {$and:[{ $gte: [ "distance",0 ] },{ $lte: ["distance",5 ] }]}, then: 3 },
-           { case: { $gte: ["distance", 6 ] }, then: 0 }
+           { case: {$and:[{ $gte: [ "$distance",0 ] },{ $lte: ["$distance",5 ] }]}, then: 3 },
+           { case: { $gte: ["$distance", 6 ] }, then: 0 }
         ]
-     }},ridingPoints:{$cond: { if: { $lte: [ "$rides", 3 ] }, then: 5, else: 3 }},ratingPoints:{$cond: { if: { $lte: [ "$rating", 3 ] }, then: 5, else: 3 }}}},
+     }},
+     ridingPoints:{$switch: {
+        branches: [
+           { case: {$and:[{ $gte: [ "$rides",3 ] },{ $lte: [user.rides,2 ] }]},then: 3 },
+           { case: {$and:[{ $lt: [ "$rides",3 ] },{ $gt: [user.rides,2 ] }]},then: 3 },
+           { case: { $lte:[ "$rides", 3 ]}, then: 1  }
+        ]
+     }},
+     ratingPoints:{$cond: { if: { $gte: [ user.rating,"$rating"] }, then: 2, else: 0 }}}},
     {$project:{boat:1,totalPoints:{$add:['$locationPoints','$ridingPoints','$ratingPoints']}}}
     ,{$sort:{totalPoints:-1}}
 ])
